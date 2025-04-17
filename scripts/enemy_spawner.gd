e599262c008f1cbc5098bc47af4de015a7bbf833
alfonsolvg <alfonsolvgcontact@gmail.com>
2025-04-17 10:02:12 -0600

    Spawn enemy function

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 @onready var timer = $Timer
+@onready var spawn_positions = $SpawnPositions
+
+var enemy_scene = preload("res://scenes/enemy.tscn")
 
 func _ready():
 	timer.connect("timeout", _timeout)
 	
 func _timeout():
-	pass
+	spawn_enemy()
+
+func spawn_enemy():
+	var positions = spawn_positions.get_children()
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_instance.global_position = positions.pick_random().global_position
+	add_child(enemy_instance)
