bc79709a868fe5b6c1b8894ac1d844f32e3f72ab
alfonsolvg <alfonsolvgcontact@gmail.com>
2025-04-21 12:31:19 -0600

    Adding sound effects

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,11 +4,14 @@ signal died
 
 @export var speed = 300
 
+@onready var hit_sound = $HitSound
+
 func _physics_process(delta):
 	global_position.x -= speed * delta
 
 func die():
 	emit_signal("died")
+	hit_sound.play()
 	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,8 @@ var speed = 350
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
 @onready var rocket_container = $RocketContainer
+@onready var die_sound = $DieSound
+@onready var shoot_sound = $ShootSound
 
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
@@ -33,6 +35,7 @@ func _physics_process(delta):
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
 	rocket_container.add_child(rocket_instance)
+	shoot_sound.play()
 	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 50
 
@@ -40,4 +43,5 @@ func take_damage():
 	emit_signal("took_damage")
 
 func die():
+	die_sound.play()
 	queue_free()
